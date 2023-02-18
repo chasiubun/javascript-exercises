bac247c7d5d3a0e52615ca53ad1f53fa8eadffd4
chasiubun <benwaisiu@gmail.com>
2023-02-17 23:33:08 -0800

    complete reversestring

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const stringArray = string.split("");
+    stringArray.reverse();
+    let newString = stringArray.toString();
+	for (instance of newString){
+		newString = newString.replace(",","");}
+	return newString;
 };
 
 // Do not edit below this line
