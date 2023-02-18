50ab4c6c51cfdd93c67ca765d9134b20943f3293
chasiubun <benwaisiu@gmail.com>
2023-02-18 11:30:44 -0800

    complete removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,34 @@
-const removeFromArray = function() {
+const removeFromArray = function(myArray, ...num) {
+    const newArray = [];
+    myArray.forEach((item) => {
+        if (!num.includes(item)){
+            newArray.push(item);
+        }
+    });    
+    return newArray
 
-};
+} 
+//name a new array
+//for each item in array1,
+//if the value is not in the num/index, 
+//push it into the new array
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+/*v1.0
+const removeFromArray = function(myArray, ...num) {
+    const index = num - 1
+    myArray.splice(index, 1);
+    return myArray;}*/
+/*v1.1
+const removeFromArray = function(myArray, ...num) {
+    const newArray = [];
+    myArray.forEach((item) => {
+        if (!num.includes(item)){
+            newArray.push(item);
+        }
+    });
+    return newArray
+}
+*/
