2303bf67577238b59383148acfaad92e02ab71e2
chasiubun <benwaisiu@gmail.com>
2023-02-17 23:37:33 -0800

    complete repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(string, amount) {
+    if (amount < 0) {return "ERROR";}
+    count = amount;
+    let answer = '';
+    while (count > 0){
+    answer += `${string}`;
+    count--;
+    }
+    return answer;
 };
+repeatString()
+
 
 // Do not edit below this line
 module.exports = repeatString;
+
+//repeatString('hey', 3) // returns 'heyheyhey'
