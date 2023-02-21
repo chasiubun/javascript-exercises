385f97abd2eee8393a98b771104d6ecdd7d0333e
chasiubun <benwaisiu@gmail.com>
2023-02-21 12:34:21 -0800

    implement equation within code to convert fahrenheit to celsius

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,25 @@
-const convertToCelsius = function() {
+onst convertToCelsius = function(temperature) {
+  // Convert a temperature in fahrenheit to celsius.
+  // Conversion equation:
+  //    <write equation from Wiki here>
+  // inputs: 
+  //    temperature (number): in units of Fahreinheit
+  // outputs:
+  //    temperature (number): in units of Celsius
+  
+  let cel = temperature * (9/5) + 32;
+  return cel;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temperature) {
+  // Convert a temperature in CELSIUS.
+  // Conversion equation:
+  //    <write equation from Wiki here>
+  // inputs: 
+  //    temperature (number): in units of Fahreinheit
+  // outputs:
+  //    temperature (number): in units of Celsius
+
 };
 
 // Do not edit below this line
