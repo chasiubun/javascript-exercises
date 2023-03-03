b38c35880bced9d3292a5756285c23d00e08026d
chasiubun <benwaisiu@gmail.com>
2023-03-03 00:10:37 -0800

    complete all assignments

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,51 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+  let total = 0;
+  let index = 0;
+  for (num in a){
+    let newNum = (a[index])
+    total += newNum
+    index++
+  }
+  return(total)
 };
 
-const multiply = function() {
-
+const multiply = function(a){
+  let total = 1;
+  let index = 0;
+    for (num in a){
+        let newNum = (a[index])
+        total = total*newNum
+        index++
+        }
+        return total
 };
 
-const power = function() {
-	
-};
+const power = function(a, b) {
+  let total = a
+  for (i = 0; i < b - 1;i++){
+      total = total * a
+      }
+  return total
+  };
 
-const factorial = function() {
-	
-};
+const factorial = function(a) {
+total = a;
+index = 1
+if (total == 0 || total == 1){return 1}
+else{
+    for (i = 0; i < (a - 1);i++){
+        total = total * (a-index)
+        index++
+    }}
+  return total}
 
 // Do not edit below this line
 module.exports = {
